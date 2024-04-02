7e8130581ca73ce8d83ef5bb989aa472fa48efad
garazh228 <adylbeksadykbekov.9.02@gmail.com>
2024-04-02 12:29:27 +0600

    Initial Commit

---
+++ Coffe_House/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coffe_House
+//
+//  Created by adyl CEO on 02/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
